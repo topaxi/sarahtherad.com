ce7ddfa41cffc24a413b305adaf5f597a0eb5769
Damian Senn <senn@puzzle.ch>
2017-07-19 10:37:34 +0200

    Deduplicate graphics dummy data

--- app/routes/graphics/post.js
+++ app/routes/graphics/post.js
@@ -2,25 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model({ slug }) {
-    return [
-      {
-        id: 2,
-        type: 'graphics',
-        slug: 'paul-stanley',
-        width: 224,
-        height: 300,
-        src: 'https://wordpress-test.sarahtherad.com/wp-content/uploads/2017/04/paul-stanley.jpg',
-        title: 'Paul Stanley',
-      },
-      {
-        id: 3,
-        type: 'graphics',
-        slug: 'sleepy-topaxi-cat',
-        width: 646,
-        height: 442,
-        src: '/pictures/sleepy-ninja.svg',
-        title: 'Sleepy topaxi cat',
-      },
-    ].find(graphic => graphic.slug === slug);
+    return this.modelFor('graphics').find(graphic => graphic.slug === slug);
   },
 });
