019bc411fbb84eee6b5a0aef50a6ebdef7749666
Damian Senn <senn@puzzle.ch>
2017-08-03 15:31:16 +0200

    Graphics post jsonld

--- app/routes/graphics/post.js
+++ app/routes/graphics/post.js
@@ -8,4 +8,31 @@ export default Route.extend({
   model({ slug }) {
     return this.rad.graphic(slug)
   },
+
+  afterModel(model) {
+    let publisher = 'sarahtherad.com'
+    let url = `https://${publisher}/blog/${model.slug}`
+
+    this.set('headData.jsonld', {
+      '@context': 'http://schema.org',
+      '@type': 'VisualArtwork',
+      publisher,
+      'author': {
+        '@type': 'Person',
+        name: 'Sarahtherad',
+        image: '',
+        url: `https://${publisher}/`,
+        sameAs: 'https://twitter.com/sarah_therad'
+      },
+      'headline': model.title,
+      url,
+      image: model.pictures[0].src,
+      datePublished: model.date,
+      dateModified: model.modified,
+      //keywords: 'ES2016, ES2017',
+      description: model.description,
+    })
+    this.set('headData.url', url)
+    this.set('headData.description', model.content)
+  },
 })
