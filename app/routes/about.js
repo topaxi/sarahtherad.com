696cd4974c3854fc44562731e4c6d51ce17a1de6
Damian Senn <senn@puzzle.ch>
2017-08-04 18:38:02 +0200

    jsonld for about

--- app/routes/about.js
+++ app/routes/about.js
@@ -4,6 +4,18 @@ const { Route } = Ember
 
 export default Route.extend({
   afterModel() {
+    let publisher = 'sarahtherad.com'
+    let url = `https://${publisher}/about`
+
+    this.set('headData.jsonld', {
+      '@context': 'http://schema.org',
+      '@type': 'Person',
+      'sameAs': 'https://twitter.com/sarah_therad'
+      publisher,
+      url,
+    })
+    this.set('headData.url', url)
+
     return this.radBackground.clear()
   },
 })
