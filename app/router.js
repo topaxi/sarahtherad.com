d1f084aa5f8b0297eb19190ea24670f031012c6e
Damian Senn <senn@puzzle.ch>
2017-08-04 19:34:21 +0200

    Remove unused getParentRoute method

--- app/router.js
+++ app/router.js
@@ -36,22 +36,6 @@ Route.reopen({
     if (routeName === 'index') routeName = 'home';
     this.get('rootclass').remove(routeName)
   },
-  getParentRoute() {
-    let route = this;
-    let handlerInfos = route.router.router.currentHandlerInfos;
-    let parent, current;
-
-    for (let i = 0; i < handlerInfos.length; i++) {
-      current = handlerInfos[i].handler;
-      if (current.routeName === route.routeName ||
-          (current.routeName.match(/./) &&
-            current.routeName.split('.')[1] === route.routeName)) {
-        return parent.routeName;
-      }
-
-      parent = current;
-    }
-  }
 })
 
 Router.map(function() {
