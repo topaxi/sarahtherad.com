7d1dee000a8d669646bfdbcc96efa79d1cec1528
Damian Senn <senn@puzzle.ch>
2017-09-08 09:30:25 +0200

    Fix jsonld on about page

--- app/routes/about.js
+++ app/routes/about.js
@@ -2,12 +2,12 @@ import Route from '@ember/routing/route';
 
 export default Route.extend({
   afterModel() {
-    let publisher = 'sarahtherad.com'
-    let url = `https://${publisher}/about`
+    let url = 'https://sarahtherad.com/about'
 
     this.set('headData.jsonld', {
       '@context': 'http://schema.org',
       '@type': 'Person',
+      name: 'Sarah Christener',
       givenName: 'Sarah',
       familyName: 'Christener',
       email: 'info@sarahtherad.com',
@@ -16,7 +16,6 @@ export default Route.extend({
         'https://twitter.com/sarah_therad/',
         'https://www.instagram.com/sarah_therad/',
       ],
-      publisher,
       url,
     })
     this.set('headData.url', url)
