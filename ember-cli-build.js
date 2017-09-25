9d499296ca48b6bcddd8c047c64edd2a2b430819
Damian Senn <senn@puzzle.ch>
2017-09-25 16:47:40 +0200

    Exclude jobs folder

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -26,6 +26,7 @@ module.exports = function(defaults) {
         /favicon.ico$/,
         /favicon-.*$/,
         /app-logos\//,
+        /jobs\//,
         /\.xml/, // any xml files (sitemaps)
       ],
     },
