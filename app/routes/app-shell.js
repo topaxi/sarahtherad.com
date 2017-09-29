171a8a85ac802df35efd4cbfdf15469fc5715d0b
Damian Senn <senn@puzzle.ch>
2017-09-29 16:28:10 +0200

    Do not load background image on app shell

--- app/routes/app-shell.js
+++ app/routes/app-shell.js
@@ -1,10 +1,7 @@
 import Route from '@ember/routing/route'
 
 export default Route.extend({
-  model() {
-    return this.radBackground.reload({
-      url: '/assets/pictures/about-background.jpeg',
-      color: '#fff',
-    })
-  },
+  beforeModel() {
+    this.radBackground.set('color', '#fff')
+  }
 })
