a7dae9fbb83abe40f25781652a3055924b2e24ee
Damian Senn <senn@puzzle.ch>
2017-09-29 16:59:57 +0200

    Fix app-shell issues with inline SVG

--- app/routes/app-shell.js
+++ app/routes/app-shell.js
@@ -2,6 +2,6 @@ import Route from '@ember/routing/route'
 
 export default Route.extend({
   beforeModel() {
-    this.radBackground.set('color', '#fff')
+    this.radBackground.set('color', '#000')
   }
 })
