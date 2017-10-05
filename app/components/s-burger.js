ddfb7556c4c70f640da1c2cb2a1d8bfdde3996c3
Damian Senn <senn@puzzle.ch>
2017-10-05 16:46:37 +0200

    Accessible menu

--- app/components/s-burger.js
+++ app/components/s-burger.js
@@ -2,13 +2,24 @@ import Component from '@ember/component';
 import { computed } from '@ember/object';
 
 export default Component.extend({
+  tagName: 'button',
+
   classNames: 'hamburglar',
   classNameBindings: [ 'open:is-open', 'closed:is-closed' ],
+  attributeBindings: [
+    'aria-haspopup',
+    'accesskey',
+    '_ariaExpanded:aria-expanded',
+  ],
 
   touched: false,
 
   closed: computed('open', 'touched', function() {
     return this.get('touched') && !this.get('open')
+  }),
+
+  _ariaExpanded: computed('open', function() {
+    return this.get('open') ? 'true' : 'false';
   }),
 
   click() {
