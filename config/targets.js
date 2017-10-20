613b3798653010b1cae7d2062b59c51e1df6cbe8
Damian Senn <senn@puzzle.ch>
2017-10-20 09:26:00 +0200

    Tweak config/targets

--- config/targets.js
+++ config/targets.js
@@ -1,12 +1,12 @@
 /* eslint-env node */
 module.exports = {
   browsers: [
-    '> 1% in CH',
+    '> 0.5% in CH',
     'ie 11',
     'last 2 Edge versions',
     'last 2 Chrome versions',
     'last 2 Firefox versions',
     'last 2 Safari versions',
     'Firefox ESR',
-  ]
-};
+  ],
+}
