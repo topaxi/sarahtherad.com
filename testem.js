951ae38dca946c50cf3b0aa2bfb6e2562516b804
Damian Senn <senn@puzzle.ch>
2017-10-29 12:35:27 +0100

    Run firefox in CI too

--- testem.js
+++ testem.js
@@ -2,21 +2,25 @@
 module.exports = {
   test_page: 'tests/index.html?hidepassed',
   disable_watching: true,
+  parallel: 2,
   launch_in_ci: [
-    'Chrome'
+    'Chrome',
+    'Firefox',
   ],
-  launch_in_dev: [
-    'Chrome'
-  ],
+  launch_in_dev: ['Chrome'],
   browser_args: {
+    Firefox: {
+      mode: 'ci',
+      args: ['-headless'],
+    },
     Chrome: {
       mode: 'ci',
       args: [
         '--disable-gpu',
         '--headless',
         '--remote-debugging-port=9222',
-        '--window-size=1440,900'
-      ]
+        '--window-size=1440,900',
+      ],
     },
-  }
-};
+  },
+}
--- tests/integration/components/pdf-viewer-test.js
+++ tests/integration/components/pdf-viewer-test.js
@@ -1,35 +1,48 @@
-import { moduleForComponent, test } from 'ember-qunit';
-import hbs from 'htmlbars-inline-precompile';
+import { moduleForComponent, test } from 'ember-qunit'
+import hbs from 'htmlbars-inline-precompile'
 
 export default function hasPdfjs() {
-  return (/Firefox/.test(window.userAgent) || window.chrome) &&
-    !/Android/.test(window.userAgent) && !/iPad|iPhone/.test(window.userAgent)
+  return (
+    (/Firefox/.test(navigator.userAgent) || navigator.chrome) &&
+    !/Android/.test(navigator.userAgent) &&
+    !/iPad|iPhone/.test(navigator.userAgent)
+  )
 }
 
 moduleForComponent('pdf-viewer', 'Integration | Component | pdf viewer', {
-  integration: true
-});
+  integration: true,
+})
 
 test('it renders', function(assert) {
-  this.render(hbs`{{pdf-viewer}}`);
+  this.render(hbs`{{pdf-viewer}}`)
 
-  assert.equal(this.$().text().trim(), '');
+  assert.equal(
+    this.$()
+      .text()
+      .trim(),
+    '',
+  )
 
   this.render(hbs`
     {{#pdf-viewer}}
       template block text
     {{/pdf-viewer}}
-  `);
+  `)
 
-  assert.equal(this.$().text().trim(), '');
-});
+  assert.equal(
+    this.$()
+      .text()
+      .trim(),
+    '',
+  )
+})
 
 test('it works', function(assert) {
   this.render(hbs`{{pdf-viewer src="about:blank"}}`)
 
-  let url = hasPdfjs() ?
-    'about:blank' :
-    'https://docs.google.com/viewer?url=about%3Ablank&embedded=true'
+  let url = hasPdfjs()
+    ? 'about:blank'
+    : 'https://docs.google.com/viewer?url=about%3Ablank&embedded=true'
 
   assert.equal(this.$('iframe').length, 1)
   assert.equal(this.$('iframe').attr('src'), url)
